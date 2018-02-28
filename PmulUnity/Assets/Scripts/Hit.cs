851083c6bf5e50b1bf214c29a0fb295b75605148
DAMMauro <maurollr6@gmail.com>
2018-02-28 19:46:35 +0100

    Probando
    Prueba

---
+++ PmulUnity/Assets/Scripts/Hit.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hit : MonoBehaviour
+{
+
+    private Animator animator;
+
+    private Vector3 direction;
+
+    private Stats estadisticasPropia;
+    private Stats estadisticasRival;
+
+    private EnemyHit enemy;
+
+    private bool movement = true;
+
+    private bool dead = false;
+
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        animator.SetBool("InCombat", true);
+        estadisticasPropia = GetComponent<Stats>();
+        
+        direction = new Vector3(estadisticasPropia.Speed, 0, 0);
+        if (estadisticasPropia.Speed >= 1)
+        {
+            animator.Play("Run");
+        }
+
+    }
+
+    private void Update()
+    {
+        if (movement)
+        {
+            transform.position += direction * Time.deltaTime;
+        }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Enemy"))
+        {
+            movement = false;
+            animator.speed = 2;
+            animator.Play("Attack");
+            estadisticasRival = collision.gameObject.GetComponent<Stats>();
+            enemy = collision.gameObject.GetComponent<EnemyHit>();
+        }
+    }
+
+    public void IsDead()
+    {
+        animator.SetBool("InCombat", false);
+        animator.Play("Die");
+        dead = true;
+        Destroy(GetComponent<Collider2D>());
+        Destroy(GetComponent<Rigidbody2D>());
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag.Equals("Enemy"))
+        {
+            if (!dead)
+            {
+                movement = true;
+                animator.Play("Run");
+                animator.speed = 1;
+            }
+        }
+    }
+    private void HitEvent()
+    {
+        if (estadisticasRival.GetAttack(estadisticasPropia.Attack()))
+        {
+            enemy.IsDead();
+        }
+    }
+
+}
---
+++ PmulUnity/Assets/Scripts/Stats.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+
+    [SerializeField]
+    private int damage = 0;
+    [SerializeField]
+    private int health = 0;
+    [SerializeField]
+    private float speed = 0.2f;
+
+
+
+    void Start()
+    {
+
+    }
+
+    //¿El golpe lo mató? 
+    //Si = true
+    //No = False
+    public bool GetAttack(int damage)
+    {
+        health -= damage;
+
+
+        if (health <= 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public int Attack()
+    {
+        return damage;
+    }
+
+    public float Speed
+    {
+        get
+        {
+            return speed;
+        }
+
+        set
+        {
+            speed = value;
+        }
+    }
+}
