1ebf7b35b8a2d65402b4c0e90b705e1556ba01a0
mauroLR <maurollr6@gmail.com>
2018-03-14 14:40:19 +0100

    Sistema de rondas y despliege de los caballeros
    ta wena esta wea

--- PmulUnity/Assets/Waves.cs
+++ PmulUnity/Assets/Waves.cs
@@ -1,17 +1,67 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Waves : MonoBehaviour {
+public class Waves : MonoBehaviour
+{
     [SerializeField]
     private Wave[] waves;
-	
-	void Start () {
-		
-	}
-	
-	
-	void Update () {
-		
-	}
+
+    [SerializeField]
+    private float timeWaves;
+
+
+    private int actualWave;
+    private int actualMob;
+    private float[] timeSpawns;
+    private float timeWavesSave;
+    void Start()
+    {
+        actualWave = 0;
+        actualMob = 0;
+        int i = 0;
+        timeWavesSave = timeWaves;
+        timeSpawns = new float[waves.Length];
+        foreach (Wave w in waves)
+        {
+            timeSpawns[i] = w.timeSpawn;
+            i++;
+        }
+    }
+
+
+    void Update()
+    {      
+        if (actualWave < waves.Length)
+        {           
+            if (actualMob < waves[actualWave].enemys.Length)
+            {
+                waves[actualWave].timeSpawn -= Time.deltaTime;
+                if (waves[actualWave].timeSpawn < 0)
+                {
+                    Instantiate(waves[actualWave].enemys[actualMob++]).transform.position = transform.position;
+                    waves[actualWave].timeSpawn = timeSpawns[actualWave];
+                }
+            }
+            else
+            {
+                timeWaves -= Time.deltaTime;
+                if (timeWaves < 0)
+                {
+                    timeWaves = timeWavesSave;
+                    actualMob = 0;
+                    actualWave++;
+                }
+            }
+        }
+
+    }
+
+    [System.Serializable]
+    public class Wave
+    {
+        public GameObject[] enemys;
+        public float timeSpawn;
+    }
 }
